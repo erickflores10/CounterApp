4071039bd259a00fd82c7669778b017227d72504
erickflores10 <developerwebmythos@gmail.com>
2021-11-23 09:33:11 -0500

    Archivos iniciales borrados y creacion de PrimeraApp

---
+++ src/PrimeraApp.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const PrimeraApp = ({saludo}) => {
+    return(
+        <Fragment>
+            <h1>{ saludo }</h1>
+            <p>Mi primera aplicacion</p>
+        </Fragment>
+    );
+}
+
+
+export default PrimeraApp;
